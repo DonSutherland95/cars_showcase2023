dfa2066bad373644674a31d3a57d79a3c5cb37e6
Sheldon Sutherland <58916345+DonSutherland95@users.noreply.github.com>
2023-12-04 22:15:10 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components";
 import Image from "next/image";
 // fix imports
+// add more advanced styling
 export default function Home() {
   return (
     <main className="overflow-hidden">
